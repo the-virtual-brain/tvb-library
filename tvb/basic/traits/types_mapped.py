0433ad31946381e44f539986537e3bdf4441355f
lia.domide <lia.domide@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2013-03-09 21:25:06 +0000

    TVB-851 Review usage of MappedType base class.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/simulator_library@4617 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

---
+++ tvb/basic/traits/types_mapped.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+#
+# (c)  Baycrest Centre for Geriatric Care ("Baycrest"), 2012, all rights reserved.
+#
+# No redistribution, clinical use or commercial re-sale is permitted.
+# Usage-license is only granted for personal or academic usage.
+# You may change sources for your private or academic use.
+# If you want to contribute to the project, you need to sign a contributor's license. 
+# Please contact info@thevirtualbrain.org for further details.
+# Neither the name of Baycrest nor the names of any TVB contributors may be used to endorse or 
+# promote products or services derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY BAYCREST ''AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, 
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL BAYCREST BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+#
+#
+
+"""
+.. moduleauthor:: Lia Domide <lia.domide@codemart.ro>
+"""
+
+from tvb.basic.config.settings import TVBSettings
+
+if TVBSettings.TRAITS_CONFIGURATION.use_storage:
+    import tvb.core.traits.types_mapped as mapped
+    MappedType = mapped.MappedType
+else:
+    from tvb.basic.traits.types_mapped import MappedTypeLight
+    MappedType = MappedTypeLight
+    
+   
+   
+    
