ddee9cc0cd418f4d1ba3a42a9f1a9faffda596f9
paula.sanz-leon <paula.sanz-leon@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2013-04-29 16:12:43 +0000

    Update Larter model and the demo making use of a contributed model
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/scientific_library@4805 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- contrib/simulator/demos/region_deterministic_contributed_model.py
+++ contrib/simulator/demos/region_deterministic_contributed_model.py
@@ -1,42 +1,22 @@
 # -*- coding: utf-8 -*-
 
 """
-Demonstrate using a 'contributed' model at the region level, deterministic
-integration scheme.
+Use a 'contributed' model
 
-.. moduleauthor:: Paula Sanz Leon <Paula@tvb.invalid>
 .. moduleauthor:: Stuart A. Knock <Stuart@tvb.invalid>
+.. moduleauthor:: Paula Sanz Leon <Paula@tvb.invalid>
 
 """
 
 # Third party python libraries
 import numpy
 
-
 # Try and import from "The Virtual Brain"
-try:
-    from tvb.basic.logger.builder import get_logger
-    LOG = get_logger(__name__)
-except ImportError:
-    import logging
-    logging.basicConfig(level=logging.DEBUG)
-    LOG = logging.getLogger(__name__)
-    LOG.warning("Failed to import TVB logger, using Python logging directly...")
-
-#Import from tvb.simulator modules:
-import tvb.simulator.simulator as simulator
-import tvb.simulator.coupling as coupling
-import tvb.simulator.integrators as integrators
-import tvb.simulator.monitors as monitors
-import tvb.datatypes.connectivity as connectivity
-
+from tvb.simulator.lab import *
 
 # Import the contributed model
-# NOTE: the 'externals/contrib/simulator/models' folder should be added to the pythonpath
-# >> export PYTHONPATH=/Users/paupau/trunk/externals/contrib/simulator/models:$PYTHONPATH
-
-
-import BrunelWang as MODEL
+# NOTE: Add the ~/scientific_library/contrib/simulator/models folder to the PYTHONPATH env variable.
+import larter as TheModel
 
 ##----------------------------------------------------------------------------##
 ##-                      Perform the simulation                              -##
@@ -44,36 +24,37 @@ import BrunelWang as MODEL
 
 LOG.info("Configuring...")
 #Initialise a Model, Coupling, and Connectivity.
-bw = MODEL.BrunelWang()
+lar = TheModel.Larter()
 
 white_matter = connectivity.Connectivity()
 white_matter.speed = numpy.array([4.0])
 
-white_matter_coupling = coupling.Linear(a=0.09)
+white_matter_coupling = coupling.Linear(a=0.0154)
 
 #Initialise an Integrator
-heunint = integrators.HeunDeterministic(dt=2**-5)
+heunint = integrators.HeunDeterministic(dt=2**-4)
 
 #Initialise some Monitors with period in physical time
-momo = monitors.Raw()
-mama = monitors.TemporalAverage(period=2**-2)
+mon_raw  = monitors.Raw()
+mon_tavg = monitors.TemporalAverage(period=2**-2)
 
 #Bundle them
-what_to_watch = (momo, mama)
+what_to_watch = (mon_raw, mon_tavg)
 
 #Initialise a Simulator -- Model, Connectivity, Integrator, and Monitors.
-sim = simulator.Simulator(model = bw, connectivity = white_matter,
+sim = simulator.Simulator(model = lar, 
+                          connectivity = white_matter,
                           coupling = white_matter_coupling, 
-                          integrator = heunint, monitors = what_to_watch)
+                          integrator = heunint, 
+                          monitors = what_to_watch)
 
 sim.configure()
 
 LOG.info("Starting simulation...")
 #Perform the simulation
-raw_data = []
-raw_time = []
-tavg_data = []
-tavg_time = []
+raw_data, raw_time = [], []
+tavg_data, tavg_time = [], []
+
 for raw, tavg in sim(simulation_length=2**8):
     if not raw is None:
         raw_time.append(raw[0])
@@ -89,28 +70,24 @@ LOG.info("Finished simulation.")
 ##-               Plot pretty pictures of what we just did                   -##
 ##----------------------------------------------------------------------------##
 
-#Plot defaults in a few combinations
-import matplotlib.pyplot as pyplot
-
 #Make the lists numpy.arrays for easier use.
 RAW = numpy.array(raw_data)
 TAVG = numpy.array(tavg_data)
 
 #Plot raw time series
-pyplot.figure(1)
-pyplot.plot(raw_time, RAW[:, 0, :, 0])
-pyplot.title("Raw -- State variable 0")
+figure(1)
+plot(raw_time, RAW[:, 0, :, 0])
+title("Raw -- State variable 0")
 
-pyplot.figure(2)
-pyplot.plot(raw_time, RAW[:, 1, :, 0])
-pyplot.title("Raw -- State variable 1")
+figure(2)
+plot(raw_time, RAW[:, 1, :, 0])
+title("Raw -- State variable 1")
 
 #Plot temporally averaged time series
-pyplot.figure(3)
-pyplot.plot(tavg_time, TAVG[:, 0, :, 0])
-pyplot.title("Temporal average")
+figure(3)
+plot(tavg_time, TAVG[:, 0, :, 0])
+title("Temporal average")
 
 #Show them
-pyplot.show()
-
+show()
 ###EoF###
