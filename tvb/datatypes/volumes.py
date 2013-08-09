c4fc8f79bf505c5185a5c79ac8a2752565282b66
lia.domide <lia.domide@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2013-08-09 09:08:47 +0000

    TVB-971 Update TVB citation text after publish.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/scientific_library@5013 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb/datatypes/volumes.py
+++ tvb/datatypes/volumes.py
@@ -24,7 +24,7 @@
 #   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
 #   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
 #       The Virtual Brain: a simulator of primate brain network dynamics.
-#   Frontiers in Neuroinformatics (in press)
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
 #
 #
 
