a7a1c8ed21d329139f05da5236b4f10358b68766
paula.sanz-leon <paula.sanz-leon@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2013-11-08 11:56:03 +0000

    Update balloon model...
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/scientific_library@5234 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb/analyzers/fmri_balloon.py
+++ tvb/analyzers/fmri_balloon.py
@@ -23,34 +23,33 @@
 #
 #   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
 #   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
-#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   The Virtual Brain: a simulator of primate brain network dynamics.
 #   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
 #
 #
 
 """
-Implementation of differet BOLD signal models as described in [Stephan2007]_
-
-Four different models are distinguishe: 
+Implementation of differet BOLD signal models. Four different models are distinguished: 
 
 + CBM_N: Classical BOLD Model Non-linear
 + CBM_L: Classical BOLD Model Linear
 + RBM_N: Revised   BOLD Model Non-linear (default)
 + RBM_L: Revised   BOLD Model Linear
 
-Classical mean that the coefficients used to compute the BOLD signal are
-derived as described in [Buxton1998]_ and [Friston2000]_
+``Classical`` means that the coefficients used to compute the BOLD signal are
+derived as described in [Obata2004]_ . ``Revised`` coefficients are defined in
+[Stephan2007]_
 
+References:
 
 .. [Stephan2007] Stephan KE, Weiskopf N, Drysdale PM, Robinson PA,
                  Friston KJ (2007) Comparing hemodynamic models with 
                  DCM. NeuroImage 38: 387-401.
 
-
-.. [Buxton1998]  DOC ME
-.. [Friston2000] DOC ME
-.. [Buxton2004]  DOC ME
-.. [Obata2004]   DOC ME
+.. [Obata2004]  Obata, T.; Liu, T. T.; Miller, K. L.; Luh, W. M.; Wong, E. C.; Frank, L. R. &
+                Buxton, R. B. (2004) **Discrepancies between BOLD and flow dynamics in primary and
+                supplementary motor areas: application of the balloon model to the
+                interpretation of BOLD transients.** Neuroimage, 21:144-153
 
 .. moduleauthor:: Paula Sanz Leon <Paula@tvb.invalid>
 
@@ -74,121 +73,144 @@ class BalloonModel(core.Type):
     A class for calculating the simulated BOLD signal given a TimeSeries
     object of TVB and returning another TimeSeries object.
 
-    NOTE: a potential problem when the input is a TimeSeriesSurface.
-    TODO: add an spatial averaging for TimeSeriesSurface.
-
     The haemodynamic model parameters based on constants for a 1.5 T scanner.
 
         
     """
     
+    #NOTE: a potential problem when the input is a TimeSeriesSurface.
+    #TODO: add an spatial averaging for TimeSeriesSurface.
+
     time_series = time_series.TimeSeries(
         label = "Time Series",
         required = True,
-        doc = """The timeseries that represents the input neural activity""")
-     
+        doc = """The timeseries that represents the input neural activity""",
+        order = 1)
+    # it also sets the bold sampling period.
     dt = basic.Float(
         label = ":math:`dt`",
-        default = 0.001, 
+        default = None, 
         required = True,
-        doc = """The integration time step size for the BOLD model (s)""")
-    
-    non_linear = basic.Bool(
-        label = "Select non_linear or linear BOLD equations",
-        default = True,
-        required = True,
-        doc = """Select the non-linear or linear set of equations for the 
-        BOLD model (N or L).""")
-
-    RBM = basic.Bool(
-        label = "Revised BOLD Model",
-        default = True,
-        required = True,
-        doc = """Select classical vs revised BOLD model (CBM or RBM). 
-        Coefficients  k1, k2 and k3 will be derived accordingly.""")
-
-    tau_s = basic.Float(
-        label = ":math:`\tau_s`",
-        default = 0.65, 
-        required = True,
-        doc = """Time of signal decay (s)""")
-
-    tau_f = basic.Float(
-        label = ":math:`\tau_f`",
-        default = 0.41, 
-        required = True,
-        doc = """Time of flow-dependent elimination or feedback regulation (s). The average 
-        time blood take to traverse the venous compartment. It is the 
-        ratio of resting blood volume (V0) to resting blood flow (F0).""")
-
-    tau_o = basic.Float(
-        label = ":math:`\tau_o`",
-        default = 0.98, 
-        required = True,
-        doc = """Haemodynamic transit time (s). The average 
-        time blood take to traverse the venous compartment. It is the 
-        ratio of resting blood volume (V0) to resting blood flow (F0).""")
-
-    alpha = basic.Float(
-        label = ":math:`\tau_f`",
-        default = 0.32, 
-        required = True,
-        doc = """Stiffness parameter. Grubb's exponent.""")
-
-
-    TE = basic.Float(
-        label = ":math:`TE`",
-        default = 0.04, 
-        required = True,
-        doc = """Echo Time""")
-
-
-    V0 = basic.Float(
-        label = ":math:`V_0`",
-        default = 4.0, 
-        required = True,
-        doc = """Resting blood volume fraction.""")
-
-    E0 = basic.Float(
-        label = ":math:`E_0`",
-        default = 0.4, 
-        required = True,
-        doc = """Resting oxygen extraction fraction.""")
-
-    epsilon = arrays.FloatArray(
-        label = ":math:`\epsilon`",
-        default = numpy.array([0.5]),
-        range = basic.Range(lo=0.5, hi=2.0, step=0.25), 
-        required = True,
-        doc = """Resting oxygen extraction fraction. In principle 
-        this parameter can be derived from empirical data 
-        and spatialized.""")
-
-
-    nu_0 = basic.Float(
-        label = ":math:`\nu_0`",
-        default = 40.3 , 
-        required = True,
-        doc = """Frequency offset at the outer surface of magnetized vessels (Hz).""")
-
-
-    r_0 = basic.Float(
-        label = ":math:`\nu_0`",
-        default = 25. , 
-        required = True,
-        doc = """Slope r0 of intravascular relaxation rate (Hz).""")
+        doc = """The integration time step size for the balloon model (s)""",
+        order = 2)
 
 
     integrator = integrators_module.Integrator(
         label = "Integration scheme",
         default = integrators_module.HeunDeterministic,
         required = True,
-        order = 6,
-        doc = """A tvb.simulator.Integrator object which is
-            an integration scheme with supporting attributes such as 
-            integration step size and noise specification for stochastic 
-            methods. It is used to compute the time courses of the model state 
-            variables.""")
+        order = 3,
+        doc = """ A tvb.simulator.Integrator object which is
+        an integration scheme with supporting attributes such as 
+        integration step size and noise specification for stochastic 
+        methods. It is used to compute the time courses of the balloon model state 
+        variables.""")
+    
+    non_linear = basic.Bool(
+        label = "Select non_linear or linear BOLD equations",
+        default = True,
+        required = True,
+        doc = """Select the non-linear or linear set of equations for the 
+        BOLD model (N or L).""",
+        order = 4)
+
+    RBM = basic.Bool(
+        label = "Revised BOLD Model",
+        default = True,
+        required = True,
+        doc = """Select classical vs revised BOLD model (CBM or RBM). 
+        Coefficients  k1, k2 and k3 will be derived accordingly.""",
+        order = 5)
+
+    neural_input_transformation = basic.Dict(
+        label="Neural input",
+        options=["none", "abs_diff", "sum"],
+        default=["none"],
+        select_multiple=False,
+        doc=""" This represents the operation to perform on the state-variable(s) of
+        the model used to generate the input TimeSeries. ``none`` takes the
+        first state-variable as neural input; `` abs_diff`` is the absolute
+        value of the derivative (first order difference); ``sum``: sum all the
+        state-variables in the input TimeSeries.""")
+
+    tau_s = basic.Float(
+        label = ":math:`\tau_s`",
+        default = 0.65, 
+        required = True,
+        doc = """Balloon model parameter. Time of signal decay (s)""", 
+        order = 6)
+
+    tau_f = basic.Float(
+        label = ":math:`\tau_f`",
+        default = 0.41, 
+        required = True,
+        doc = """ Balloon model parameter. Time of flow-dependent elimination or
+        feedback regulation (s). The average  time blood take to traverse the
+        venous compartment. It is the  ratio of resting blood volume (V0) to
+        resting blood flow (F0).""",
+        order = 7)
+
+    tau_o = basic.Float(
+        label = ":math:`\tau_o`",
+        default = 0.98, 
+        required = True,
+        doc = """
+        Balloon model parameter. Haemodynamic transit time (s). The average
+        time blood take to traverse the venous compartment. It is the  ratio
+        of resting blood volume (V0) to resting blood flow (F0).""",
+        order = 8)
+
+    alpha = basic.Float(
+        label = ":math:`\tau_f`",
+        default = 0.32, 
+        required = True,
+        doc = """Balloon model parameter. Stiffness parameter. Grubb's exponent.""",
+        order = 9)
+
+    TE = basic.Float(
+        label = ":math:`TE`",
+        default = 0.04, 
+        required = True,
+        doc = """BOLD parameter. Echo Time""",
+        order = 10)
+
+    V0 = basic.Float(
+        label = ":math:`V_0`",
+        default = 4.0, 
+        required = True,
+        doc = """BOLD parameter. Resting blood volume fraction.""",
+        order = 11)
+
+    E0 = basic.Float(
+        label = ":math:`E_0`",
+        default = 0.4, 
+        required = True,
+        doc = """BOLD parameter. Resting oxygen extraction fraction.""",
+        order = 12)
+
+    epsilon = arrays.FloatArray(
+        label = ":math:`\epsilon`",
+        default = numpy.array([0.5]),
+        range = basic.Range(lo=0.5, hi=2.0, step=0.25), 
+        required = True,
+        doc = """ BOLD parameter. Ratio of intra- and extravascular signals. In principle  this
+        parameter could be derived from empirical data and spatialized.""",
+        order =13)
+
+    nu_0 = basic.Float(
+        label = ":math:`\nu_0`",
+        default = 40.3 , 
+        required = True,
+        doc = """BOLD parameter. Frequency offset at the outer surface of magnetized vessels (Hz).""",
+        order = 14)
+
+    r_0 = basic.Float(
+        label = ":math:`\nu_0`",
+        default = 25. , 
+        required = True,
+        doc = """ BOLD parameter. Slope r0 of intravascular relaxation rate (Hz). Only used for
+        ``revised`` coefficients. """,
+        order = 15)
 
 
 
@@ -201,37 +223,52 @@ class BalloonModel(core.Type):
         
         ts_shape = self.time_series.data.shape
 
-        # for the time being just testing with the first state variable
-        # TODO: check how many sv time_series dtype has
+        #NOTE: Just using the first state variable, although in the Bold monitor
+        #      input is the sum over the state-variables. Only time-series
+        #      from basic monitors should be used as inputs.
 
         neural_activity = self.time_series.data[:, 0, :, :]
         neural_activity = neural_activity[:, numpy.newaxis, :, :]
 
         ts_time  = self.time_series.time 
-        dt       = self.time_series.sample_period / 1000.#self.dt                 # (s) integration time step
 
-        if dt < (self.time_series.sample_period / 1000.):
+        if self.dt is None:
+            self.dt = self.time_series.sample_period / 1000. # (s) integration time step
+            msg = "Integration time step size for the balloon model is %s seconds" % str(dt)
+            LOG.info(msg)
+
+        #NOTE: Avoid upsampling ...
+        if self.dt < (self.time_series.sample_period / 1000.):
             msg = "Integration time step shouldn't be smaller than the sampling period of the input signal." 
             LOG.error()
 
-        # integration time
-        t_int  = ts_time[:-1] / 1000.            # (s)
+        # integration time in (s)
+        t_int  = ts_time / 1000. 
+        ballon_nvar = 4           
 
-        state = numpy.zeros((ts_shape[0]-1, 4, ts_shape[2], 1))
+        #NOTE: hard coded initial conditions
+        state = numpy.zeros((ts_shape[0], balloon_nvar, ts_shape[2], 1)) #s
         state[0, 1,:] = 1. # f
         state[0, 2,:] = 1. # v
         state[0, 3,:] = 1. # q
 
+        # BOLD model coefficients
         k = self.compute_derived_parameters()
         k1, k2, k3 = k[0], k[1], k[2]
 
-        self.integrator.dt = dt
+        # prepare integrator
+        self.integrator.dt = self.dt
         self.integrator.configure()
 
         scheme = self.integrator.scheme
+
+        # NOTE: the following variables are not used in this integration but
+        # required due to the way integrators scheme has been defined.
+
         local_coupling = 0.0
         stimulus = 0.0
 
+        # solve equations
         for step in range(1, t_int.shape[0]):
             state[step, :] = scheme(state[step-1, :], self.balloon_dfun, neural_activity[step, :], local_coupling, stimulus)
 
@@ -241,6 +278,7 @@ class BalloonModel(core.Type):
         v = state[:, 2,:] 
         q = state[:, 3,:] 
 
+        # BOLD models
         if self.non_linear:
             """
             Non-linear BOLD model equations.
@@ -313,12 +351,9 @@ class BalloonModel(core.Type):
 
         x = neural_input[0,:]
 
-        ds  = x - (1./self.tau_s) * s - (1./self.tau_f) * (f - 1 ) 
+        ds  = x - (1./self.tau_s) * s - (1./self.tau_f) * (f-1) 
         df  = s
         dv  = (1. / self.tau_o) * (f - v**(1./self.alpha))
         dq  = (1. / self.tau_o) * ((f * (1.-(1. - self.E0)**(1./f)) / self.E0) - (v**(1./self.alpha)) * (q/v))  
 
         return numpy.array([ds, df, dv, dq])
-
-
-
