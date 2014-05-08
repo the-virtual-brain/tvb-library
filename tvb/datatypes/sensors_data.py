bf7c02fed5bf54b64f769a26634cda9cbf2b9c02
tim <tim@timmy>
2014-05-08 19:18:00 +0200

    delete orientations default attribute

--- tvb/datatypes/sensors_data.py
+++ tvb/datatypes/sensors_data.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 #
-#  TheVirtualBrain-Scientific Package. This package holds all simulators, and 
+#  TheVirtualBrain-Scientific Package. This package holds all simulators, and
 # analysers necessary to run brain-simulations. You can use it stand alone or
 # in conjunction with TheVirtualBrain-Framework Package. See content of the
 # documentation-folder for more details. See also http://www.thevirtualbrain.org
 #
 # (c) 2012-2013, Baycrest Centre for Geriatric Care ("Baycrest")
 #
-# This program is free software; you can redistribute it and/or modify it under 
+# This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License version 2 as published by the Free
 # Software Foundation. This program is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
-# License for more details. You should have received a copy of the GNU General 
+# License for more details. You should have received a copy of the GNU General
 # Public License along with this program; if not, you can download it here
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0
 #
@@ -58,7 +58,7 @@ INTERNAL_POLYMORPHIC_IDENTITY = "Internal"
 class SensorsData(MappedType):
     """
     Base Sensors class.
-    All sensors have locations. 
+    All sensors have locations.
     Some will have orientations, e.g. MEG.
     """
 
@@ -80,8 +80,6 @@ class SensorsData(MappedType):
 
     has_orientation = basic.Bool(default=False)
 
-    orientations = arrays.OrientationArray(required=False)
-
     number_of_sensors = basic.Integer(
         label="Number of sensors",
         doc="""The number of sensors described by these Sensors.""")
@@ -93,13 +91,13 @@ class SensorsEEGData(SensorsData):
     EEG sensor locations are represented as unit vectors, these need to be
     combined with a head(outer-skin) surface to obtain actual sensor locations
     ::
-        
+
                               position
                                  |
                                 / \\
                                /   \\
         file columns: labels, x, y, z
-        
+
     """
     _ui_name = "EEG Sensors"
 
@@ -124,13 +122,13 @@ class SensorsMEGData(SensorsData):
     These are actually just SQUIDS. Axial or planar gradiometers are achieved
     by calculating lead fields for two sets of sensors and then subtracting...
     ::
-        
+
                               position  orientation
                                  |           |
                                 / \         / \\
                                /   \       /   \\
         file columns: labels, x, y, z,   dx, dy, dz
-        
+
     """
     _ui_name = "MEG sensors"
 
