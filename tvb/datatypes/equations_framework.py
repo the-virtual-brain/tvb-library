7bcff4581db8d83522b5c3a4e6435b9d34fe192e
mihai.andrei <mihai.andrei@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2014-05-21 07:30:46 +0000

    TVB-1361 Removed duplicate equation computing logic: the ui_equation mechanism
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/scientific_library@6027 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb/datatypes/equations_framework.py
+++ tvb/datatypes/equations_framework.py
@@ -37,15 +37,11 @@ Framework methods for the Equation datatypes.
 .. moduleauthor:: Stuart A. Knock <Stuart@tvb.invalid>
 
 """
-
-import re
 import json
-import parser
 import numpy
-import tvb.datatypes.equations_data as equations_data
-import tvb.basic.traits.parameters_factory as parameters_factory
+from tvb.datatypes import equations_data
+from tvb.basic.traits import parameters_factory
 from tvb.basic.logger.builder import get_logger
-
 
 LOG = get_logger(__name__)
 
@@ -54,34 +50,9 @@ LOG = get_logger(__name__)
 DEFAULT_PLOT_GRANULARITY = 1024
 
 
-
 class EquationFramework(equations_data.EquationData):
     """ This class exists to add framework methods to EquationData. """
     __tablename__ = None
-
-
-    @property
-    def ui_equation(self):
-        """
-        The string representation of this equation that will be used for computing the values in certain points.
-
-        Because some equations contains operations that python doesn't know how to interpret
-        we replaced those operations with others known by python.
-        E.g.: exp($expression) replaced with 2.71**($expression)
-        """
-        return self.equation
-
-
-    def get_parsed_equation(self):
-        """
-        Parses the string representation of the equation and replaces
-        all the parameters placeholders with their values.
-        """
-        equation_str = self.ui_equation
-        for param in self.parameters:
-            equation_str = re.sub('\\b' + param + '\\b', str(self.parameters[param]), equation_str)
-        return equation_str
-
 
     def get_series_data(self, min_range=0, max_range=100, step=None):
         """
@@ -91,44 +62,13 @@ class EquationFramework(equations_data.EquationData):
         if step is None:
             step = float(max_range - min_range) / DEFAULT_PLOT_GRANULARITY
 
-        from math import sin, cos, sqrt, exp
+        var = numpy.arange(min_range, max_range+step, step)
+        var = var[numpy.newaxis, :]
 
-        result = []
-        parsed_equation = self.get_parsed_equation()
-        parsed_equation = parser.expr(parsed_equation).compile()
-        last_valid_point_value = 0
-        was_exception_thrown = False
-        for var in numpy.arange(min_range, max_range + step, step):
-            try:
-                point_value = eval(parsed_equation)
-                #the number should have max 20 decimals
-                point_value = round(point_value, 19)
-                result.append([float(var), point_value])
-                last_valid_point_value = point_value
-            except OverflowError, exc:
-                LOG.exception(exc)
-                LOG.error("Could not evaluate the equation at step var = " + str(var))
-                result.append([var, last_valid_point_value])
-                was_exception_thrown = True
-        return result, was_exception_thrown
-
-
-        ## TODO Try to make the commented code bellow usable. Currently it is not.
-        # result = []
-        # try:
-        #     x_array = numpy.arange(min_range, max_range + step, step)
-        #     self.pattern = x_array
-        #     y_array = self.pattern
-        #     for i in numpy.arrange(0, x_array.size):
-        #         result.append([float(x_array[i]), round(y_array[i], 19)])
-        #
-        # except Exception, exc:
-        #     LOG.exception(exc)
-        #     LOG.error("Could not evaluate the equation")
-        #     was_exception_thrown = True
-        #
-        # return result, was_exception_thrown
-
+        self.pattern = var
+        y = self.pattern
+        result = zip(var.flat, y.flat)
+        return result, False
 
 
     @staticmethod
@@ -137,21 +77,28 @@ class EquationFramework(equations_data.EquationData):
         Builds from the given data dictionary the equation for the specified field name.
         The dictionary should have the data collapsed.
         """
-        equation = None
-        if equation_field_name in submitted_data_dict:
-            equation_type = submitted_data_dict[equation_field_name]
-            equation_parameters = {}
-            eq_param_str = equation_field_name + '_parameters'
+        if equation_field_name not in submitted_data_dict:
+            return None
 
-            if eq_param_str in submitted_data_dict and 'parameters' in submitted_data_dict[eq_param_str]:
-                equation_parameters = submitted_data_dict[eq_param_str]['parameters']
-            if eq_param_str in submitted_data_dict and 'parameters_parameters' in submitted_data_dict[eq_param_str]:
-                equation_parameters = submitted_data_dict[eq_param_str]['parameters_parameters']
-            equation = parameters_factory.get_traited_instance_for_name(equation_type, equations_data.EquationData,
-                                                                        {'parameters': equation_parameters})
-            if alter_submitted_dictionary:
-                del submitted_data_dict[eq_param_str]
-                submitted_data_dict[equation_field_name] = equation
+        eq_param_str = equation_field_name + '_parameters'
+        eq = submitted_data_dict.get(eq_param_str)
+
+        equation_parameters = {}
+        if eq:
+            if 'parameters' in eq:
+                equation_parameters = eq['parameters']
+            if 'parameters_parameters' in eq:
+                equation_parameters = eq['parameters_parameters']
+
+        for k in equation_parameters:
+            equation_parameters[k] = float(equation_parameters[k])
+
+        equation_type = submitted_data_dict[equation_field_name]
+        equation = parameters_factory.get_traited_instance_for_name(equation_type, equations_data.EquationData,
+                                                                    {'parameters': equation_parameters})
+        if alter_submitted_dictionary:
+            del submitted_data_dict[eq_param_str]
+            submitted_data_dict[equation_field_name] = equation
 
         return equation
 
@@ -186,116 +133,63 @@ class EquationFramework(equations_data.EquationData):
         modulename = loaded_dict['__mapped_module']
         classname = loaded_dict['__mapped_class']
         module_entity = __import__(modulename, globals(), locals(), [classname])
-        class_entity = eval('module_entity.' + classname)
+        class_entity = getattr(module_entity, classname)
         loaded_instance = class_entity()
         loaded_instance.parameters = loaded_dict['parameters']
         return loaded_instance
 
 
-
 class DiscreteEquationFramework(equations_data.DiscreteEquationData, EquationFramework):
     """ This class exists to add framework methods to DiscreteData """
-    pass
-
 
 
 class LinearFramework(equations_data.LinearData, EquationFramework):
     """ This class exists to add framework methods to LinearData """
-    pass
-
 
 
 class GaussianFramework(equations_data.GaussianData, EquationFramework):
     """ This class exists to add framework methods to GaussianData """
 
 
-    @property
-    def ui_equation(self):
-        return "(amp * 2.71**(-((var-midpoint)**2 / (2.0 * sigma**2)))) + offset"
-
-
-
 class DoubleGaussianFramework(equations_data.DoubleGaussianData, EquationFramework):
     """ This class exists to add framework methods to DoubleGaussianData """
-
-
-    @property
-    def ui_equation(self):
-        return "(amp_1 * 2.71**(-((var-midpoint_1)**2 / (2.0 * sigma_1**2)))) - (amp_2 * 2.71**(-((var-midpoint_2)**2 / (2.0 * sigma_2**2))))"
-
 
 
 class SigmoidFramework(equations_data.SigmoidData, EquationFramework):
     """ This class exists to add framework methods to SigmoidData """
 
 
-    @property
-    def ui_equation(self):
-        return "(amp / (1.0 + 2.71**(-1.8137993642342178 * (radius-var)/sigma))) + offset"
-
-
-
 class GeneralizedSigmoidFramework(equations_data.GeneralizedSigmoidData, EquationFramework):
     """ This class exists to add framework methods to GeneralizedSigmoidData """
 
 
-    @property
-    def ui_equation(self):
-        return "low + (high - low) / (1.0 + 2.71**(-1.8137993642342178 * (var-midpoint)/sigma))"
-
-
-
 class SinusoidFramework(equations_data.SinusoidData, EquationFramework):
     """ This class exists to add framework methods to SinusoidData """
-    pass
-
 
 
 class CosineFramework(equations_data.CosineData, EquationFramework):
     """ This class exists to add framework methods to CosineData """
-    pass
-
 
 
 class AlphaFramework(equations_data.AlphaData, EquationFramework):
     """ This class exists to add framework methods to AlphaData """
 
 
-    @property
-    def ui_equation(self):
-        return "(alpha * beta) / (beta - alpha) * (2.71**(-alpha * (var-onset)) - 2.71**(-beta * (var-onset))) if (var-onset) > 0 else  0.0 * var"
-
-
-
 class PulseTrainFramework(equations_data.PulseTrainData, EquationFramework):
     """ This class exists to add framework methods to PulseTrainData """
 
 
-    @property
-    def ui_equation(self):
-        return "amp if (var % T) <= tau and var >= onset else 0.0 * var"
-
-
-
 class GammaFramework(equations_data.GammaData, EquationFramework):
     """ This class exists to add framework methods to GammaData """
-    pass
-
 
 
 class DoubleExponentialFramework(equations_data.DoubleExponentialData, EquationFramework):
     """ This class exists to add framework methods to GammaData """
-    pass
-
 
 
 class FirstOrderVolterraFramework(equations_data.DoubleExponentialData, EquationFramework):
     """ This class exists to add framework methods to GammaData """
-    pass
 
 
 class MixtureOfGammasFramework(equations_data.MixtureOfGammasData, EquationFramework):
     """ This class exists to add framework methods to GammaData """
-    pass
-    
-    
