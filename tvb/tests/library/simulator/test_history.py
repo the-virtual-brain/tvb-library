97c16262554070b36c09970b14b7062582981f72
maedoc <maedoc@mm.st>
2015-01-20 00:22:52 +0100

    finish exact propagation test

--- tvb/tests/library/simulator/test_history.py
+++ tvb/tests/library/simulator/test_history.py
@@ -10,7 +10,8 @@ import numpy as np
 try:
     from tvb.tests.library.base_testcase import BaseTestCase
 except:
-    pass
+    BaseTestCase = object
+
 from tvb.simulator import simulator as sim
 
 
@@ -32,10 +33,10 @@ class Sum(sim.models_module.Model):
 class ExactPropagationTests(BaseTestCase):
 
     def build_simulator(self, n=4):
-        conn = np.zeros((n, n), np.int32)
-        for i in range(conn.shape[0] - 1):
-            conn[i, i+1] = 1
-        dist = np.r_[:conn.size].reshape(conn.shape)
+        self.conn = np.zeros((n, n), np.int32)
+        for i in range(self.conn.shape[0] - 1):
+            self.conn[i, i+1] = 1
+        self.dist = np.r_[:n*n].reshape((n, n))
         self.sim = sim.Simulator(
             conduction_speed=1,
             coupling=IdCoupling(),
@@ -45,8 +46,8 @@ class ExactPropagationTests(BaseTestCase):
             initial_conditions=np.ones((n*n, 1, n, 1)),
             simulation_length=10,
             connectivity=sim.connectivity_dtype.Connectivity(
-                weights=conn,
-                tract_lengths=dist
+                weights=self.conn,
+                tract_lengths=self.dist
             ),
             model=Sum(),
             monitors=(sim.monitors_module.Raw(), ),
@@ -56,18 +57,19 @@ class ExactPropagationTests(BaseTestCase):
     def test_propagation(self):
         n = 4
         self.build_simulator(n=n)
-        if False:
-            print self.sim.connectivity.weights
-            print self.sim.connectivity.delays
         x = np.zeros((n, ))
         xs = []
         for (t, raw), in self.sim():
             xs.append(raw.flat[:].copy())
         xs = np.array(xs)
-        # manually propgate information
-        xs_ = np.ones_like(xs)
-        xs_[:, :3] += np.cumsum(xs_[:, 3], axis=0)[:, np.newaxis]
-        for i in reversed(range(n - 2)):
-            di = self.sim.connectivity.delays[i, i + 1]
-            xs_[(di + 1):, i] = xs_[di, i] + np.cumsum(xs_[:len(xs_) - di - 1, i + 1])
-        assert np.allclose(xs_, xs)
+        xs_ =np.array([[  2.,   2.,   2.,   1.],
+                       [  3.,   3.,   3.,   1.],
+                       [  5.,   4.,   4.,   1.],
+                       [  8.,   5.,   5.,   1.],
+                       [ 12.,   6.,   6.,   1.],
+                       [ 17.,   7.,   7.,   1.],
+                       [ 23.,   8.,   8.,   1.],
+                       [ 30.,  10.,   9.,   1.],
+                       [ 38.,  13.,  10.,   1.],
+                       [ 48.,  17.,  11.,   1.]])
+        self.assertTrue(np.allclose(xs, xs_))
