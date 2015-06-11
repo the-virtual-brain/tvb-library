ef45ba23e3f1efc9be8ffd76179eafc561eddd06
Lia Domide <ldomide@gmail.com>
2015-06-11 22:50:40 +0200

    Cleanup wrong merge

--- tvb/datatypes/projections.py
+++ tvb/datatypes/projections.py
@@ -64,23 +64,6 @@ class ProjectionMatrix(framework.ProjectionMatrixFramework, scientific.Projectio
         return self.projection_data.shape
 
     @staticmethod
-    def load_region_projection_matrix(result, source_file):
-        source_full_path = try_get_absolute_path("tvb_data.projectionMatrix", source_file)
-        if source_file.endswith(".mat"):
-            # consider we have a brainstorm format
-            raise Exception(
-                    'Please import your Brainstorm Projection matrix by '
-                    'instantiating the surface projection Class')
-        elif source_file.endswith(".npy"):
-            # numpy array with the projectino matrix arleady computed
-            result.projection_data = numpy.load(source_full_path)
-        else:
-            raise Exception(
-                    'The projection matrix must be either a numpy array'
-                    ' or a brainstorm .mat file')
-        return result
-
-    @staticmethod
     def load_surface_projection_matrix(result, source_file):
         source_full_path = try_get_absolute_path("tvb_data.projectionMatrix", source_file)
         if source_file.endswith(".mat"):
@@ -155,35 +138,6 @@ class ProjectionSurfaceMEG(framework.ProjectionSurfaceMEGFramework,
         else:
             result = instance
         result = ProjectionMatrix.load_surface_projection_matrix(result, source_file=source_file)
-        return result 
-
-
-class ProjectionRegionSEEG(framework.ProjectionRegionMEGFramework,
-                          scientific.ProjectionRegionMEGScientific, ProjectionMatrix):
-    """
-    This class brings together the scientific and framework methods that are
-    associated with the ProjectionMatrix DataType.
-
-    ::
-
-                          ProjectionRegionMEGData
-                                     |
-                                    / \\
-        ProjectionRegionMEGFramework   ProjectionRegionMEGScientific
-                                    \ /
-                                     |
-                           ProjectionRegionMEG
-
-
-    """
-
-    @staticmethod
-    def from_file(source_file='projection_SEEG_region.npy', instance=None):
-        if instance is None:
-            result = ProjectionRegionSEEG()
-        else:
-            result = instance
-        result = ProjectionMatrix.load_region_projection_matrix(result, source_file=source_file)
         return result 
 
 
