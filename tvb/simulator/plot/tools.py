8a6b95e76e27b121cc54758416703e880edc5bff
umar abdullahi <umarbrowser20@gmail.com>
2019-07-20 06:45:02 +0100

    Implement and reuse 3D viewers#GSoC19

--- tvb/simulator/plot/tools.py
+++ tvb/simulator/plot/tools.py
@@ -765,6 +765,43 @@ def region_simulate(raw_data, tavg_data, raw_time, tavg_time):
 import matplotlib.pyplot as plt
 import numpy as np
 
+def three_d_dviewer():
+    """Viewing 3D Volumetric Data With Matplotlib"""
+    def remove_keymap_conflicts(new_keys_set):
+        for prop in plt.rcParams:
+            if prop.startswith('keymap.'):
+                keys = plt.rcParams[prop]
+                remove_list = set(keys) & new_keys_set
+                for key in remove_list:
+                    keys.remove(key)
+
+    def multi_slice_viewer(volume):
+        remove_keymap_conflicts({'j', 'k'})
+        fig, ax = plt.subplots()
+        ax.volume = volume
+        ax.index = volume.shape[0] // 2
+        ax.imshow(volume[ax.index])
+        fig.canvas.mpl_connect('key_press_event', process_key)
+
+    def process_key(event):
+        fig = event.canvas.figure
+        ax = fig.axes[0]
+        if event.key == 'j':
+            previous_slice(ax)
+        elif event.key == 'k':
+            next_slice(ax)
+        fig.canvas.draw()
+
+    def previous_slice(ax):
+        volume = ax.volume
+        ax.index = (ax.index - 1) % volume.shape[0]  # wrap around using %
+        ax.images[0].set_array(volume[ax.index])
+
+    def next_slice(ax):
+        volume = ax.volume
+        ax.index = (ax.index + 1) % volume.shape[0]
+        ax.images[0].set_array(volume[ax.index])
+
 def cube():
 
     """Demonstrates plotting 3D volumetric objects"""
