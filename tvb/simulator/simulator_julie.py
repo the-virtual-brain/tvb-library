a34ef96f3116c210e3315574495e0819aa7b3264
Dionysios Perdikis <dionperd@gmail.com>
2019-10-15 16:18:09 +0200

    Added the modified simulator for Julie's stimulus needs

---
+++ tvb/simulator/simulator_julie.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+#
+#
+#  TheVirtualBrain-Scientific Package. This package holds all simulators, and
+# analysers necessary to run brain-simulations. You can use it stand alone or
+# in conjunction with TheVirtualBrain-Framework Package. See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2017, Baycrest Centre for Geriatric Care ("Baycrest") and others
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
+
+"""
+This is the main module of the simulator. It defines the Simulator class which
+brings together all the structural and dynamic components necessary to define a
+simulation and the method for running the simulation.
+
+.. moduleauthor:: Stuart A. Knock <Stuart@tvb.invalid>
+.. moduleauthor:: Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
+.. moduleauthor:: Paula Sanz Leon <Paula@tvb.invalid>
+
+"""
+
+import math
+import numpy
+
+from tvb.simulator.simulator import Simulator as SimulatorTVB
+from .common import get_logger
+
+
+LOG = get_logger(__name__)
+
+
+# TODO with refactor, this becomes more of a builder, since iterator will account for
+# most of the runtime associated with a simulation.
+class Simulator(SimulatorTVB):
+
+    source_eeg = numpy.array([])
+
+    def __call__(self, simulation_length=None, random_state=None):
+        """
+        Return an iterator which steps through simulation time, generating monitor outputs.
+
+        See the run method for a convenient way to collect all output in one call.
+
+        :param simulation_length: Length of the simulation to perform in ms.
+        :param random_state:  State of NumPy RNG to use for stochastic integration.
+        :return: Iterator over monitor outputs.
+        """
+
+        self.calls += 1
+        if simulation_length is not None:
+            self.simulation_length = simulation_length
+
+        # intialization
+        self._guesstimate_runtime()
+        self._calculate_storage_requirement()
+        self._handle_random_state(random_state)
+        n_reg = self.connectivity.number_of_regions
+        local_coupling = self._prepare_local_coupling()
+        stimulus = self._prepare_stimulus()
+        state = self.current_state
+
+        # integration loop
+        n_steps = int(math.ceil(self.simulation_length / self.integrator.dt))
+        for step in range(self.current_step + 1, self.current_step + n_steps +1):
+            # needs implementing by history + coupling?
+            node_coupling = self._loop_compute_node_coupling(step)
+            self._loop_update_stimulus(step, stimulus)
+            self.model.I_bg = self.source_eeg[step-1].flatten()
+            state = self.integrator.scheme(state, self.model.dfun, node_coupling, local_coupling, stimulus)
+            self._loop_update_history(step, n_reg, state)
+            output = self._loop_monitor_output(step, state)
+            if output is not None:
+                yield output
+
+        self.current_state = state
+        self.current_step = self.current_step + n_steps
